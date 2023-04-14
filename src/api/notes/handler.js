e0144f3db7a819fba28984d7af45b460b00728b1
PerdiDev <perdidev@gmail.com>
2023-04-14 10:08:28 +0800

    data validation implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,7 +1,9 @@
+const ClientError = require("../../exception/ClientError");
 
 class NotesHandler {
-    constructor(service) {
+    constructor(service, validator) {
         this._service = service;
+        this._validator = validator;
 
         this.postNoteHandler = this.postNoteHandler.bind(this);
         this.getNotesHandler = this.getNotesHandler.bind(this);
@@ -12,6 +14,7 @@ class NotesHandler {
 
     postNoteHandler(request, h) {
         try {
+            this._validator.validateNotePayload(request.payload);
             const { title="untitled", body, tags } = request.payload;
     
             const noteId = this._service.addNote({title, body, tags});
@@ -27,11 +30,21 @@ class NotesHandler {
             response.code(201);
             return response;
         } catch (error) {
+            if(error instanceof ClientError) {
+                const response = h.response({
+                    status: 'fail',
+                    message: error.message
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+
             const response = h.response({
-                status: 'fail',
-                message: error.message
-            });
-            response.code(400);
+                status: 'error', 
+                message: 'Maaf, terjadi kegagalan pada server kami.',
+            })
+            response.code(500);
+            console.error(error);
             return response;
         }
     }
@@ -59,17 +72,28 @@ class NotesHandler {
                 }
             };            
         } catch (error) {
+            if(error instanceof ClientError) {
+                const response = h.response({
+                    status: 'fail',
+                    message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+
             const response = h.response({
-                status: 'fail',
-                message: error.message,
-            });
-            response.code(404);
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.'
+            })
+            response.code(500);
+            console.error(error);
             return response;
         }
     }
 
     putNoteByIdHandler(request, h) {
         try {
+            this._validator.validateNotePayload(request.payload);
             const {id} = request.params;
     
             this._service.editNoteById(id, request.payload);
@@ -80,11 +104,21 @@ class NotesHandler {
             };
             
         } catch (error) {
+            if(error instanceof ClientError) {
+                const response = h.response({
+                    status: 'fail',
+                    message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+
             const response = h.response({
-                status: 'fail',
-                message: error.message,
-            });
-            response.code(404);
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.'
+            })
+            response.code(500);
+            console.error(error);
             return response;
         }
     }
@@ -100,11 +134,21 @@ class NotesHandler {
                 message: 'Catatan berhasil dihapus'
             };
         } catch (error) {
+            if(error instanceof ClientError) {
+                const response = h.response({
+                    status: 'fail',
+                    message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+
             const response = h.response({
-                status: 'fail',
-                message: error.message,
-            });
-            response.code(404);
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.'
+            })
+            response.code(500);
+            console.error(error);
             return response;
         }
     }
